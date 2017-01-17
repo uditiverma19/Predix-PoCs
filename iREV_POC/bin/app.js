839db7e6b861446037f2d03e2b88ba501b120c97
uditiverma19 <uv00431975@techmahindra.com>
2017-01-17 16:36:59 +0530

    Add files via upload

---
+++ iREV_POC/bin/app.js
@@ -0,0 +1,50 @@
+﻿(function () {
+
+    'use strict';
+
+
+
+    angular
+
+        .module('app', ['ngRoute', 'ngCookies'])
+
+        .config(config);
+
+      /*  .run(run);*/
+
+
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+
+    function config($routeProvider, $locationProvider) {
+
+        $routeProvider
+
+        .when('/', {
+
+                controller: 'HomeCtrl',
+
+                templateUrl: 'app-view/home.view.html'/*,
+
+                controllerAs: 'vm'*/
+
+            })
+
+	    .when('/backlog', {
+
+                controller: 'BacklogCtrl',
+
+                templateUrl: 'app-view/backlog.view.html'/*,
+
+                controllerAs: 'vm'*/
+
+            })
+            .otherwise({ redirectTo: '/' });
+
+    }
+
+
+/*
+    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];*/
+
+})();
